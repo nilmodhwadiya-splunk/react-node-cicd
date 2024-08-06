b010486fa6e4f11712a016e9bbc17adc93ddb793
nilmodhwadiya-splunk <nmodhwadiya@splunk.com>
2024-08-06 10:50:36 +0800

    updated .yml file

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/names", (req, res) => {
   res.send("Modhwadiya Nilesh");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000.....");
+app.listen(5001, () => {
+  console.log("Server is running on port 5001.....");
 });
